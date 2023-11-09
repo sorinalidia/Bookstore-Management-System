3dcf2cbc64f0b296f6c2f47d29d07ee5dac7acda
sorinalidia <sorinalidiategla@gmail.com>
2023-11-09 19:30:50 +0200

    final worked

--- tema1_IS/src/main/java/org/example/Main.java
+++ tema1_IS/src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
+import database.DatabaseConnectionFactory;
+import model.*;
+import model.builder.BookBuilder;
+import repository.book.BookRepository;
+import repository.book.BookRepositoryMySQL;
 
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+
+        BookRepository bookRepository = new BookRepositoryMySQL(DatabaseConnectionFactory.getConnectionWrapper(true).getConnection());
+
+        Book book = new BookBuilder()
+                .setTitle("Harry Potter")
+                .setAuthor("J.K. Rowling")
+                .setPublishedDate(LocalDate.of(2010, 7, 3))
+                .build();
+
+        // bookRepository.save(book);
+        bookRepository.removeAll();
+        System.out.println(book);
+        System.out.println(bookRepository.findAll());
     }
 }
